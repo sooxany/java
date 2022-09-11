90afe12c233d903c3a35def57ce8f9b39b1324c9
sooxany <soojin0929@naver.com>
2022-09-11 21:12:38 +0900

    Add files via upload

---
+++ CH1/SuccessOrFail.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class SuccessOrFail {   
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("점수를 입력하세요.");
+        int score = scanner.nextInt();
+    
+        if (score >= 80)
+            System.out.println("축하합니다. 합격입니다. ");
+        else
+            System.out.println("불합격입니다.");
+
+        scanner.close();
+    }   
+
+}
+
